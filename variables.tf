355d72630546c0306f6d2b12f8a4ab7f3ff62ad1
Atisha Shaurya <94087803+Atisha-Shaurya@users.noreply.github.com>
2023-09-03 16:47:46 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,15 +1,3 @@
-# variable "azure_client_id" {
-#   default = " "
-# }
-# variable "azure_client_secret" {
-#   default = " "
-# }
-# variable "azure_tenant_id" {
-#   default = " "
-# }
-# variable "azure_subscription_id" {
-#   default = " "
-# }
 variable "resource_group_name" {
   type        = string
   description = "RG name in Azure"
